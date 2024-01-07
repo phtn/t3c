0801e15d016b05beaa70257dcce43f1a0824581a
Jun Orville Lecena <jun.lecena@comptrolla.com>
2024-01-07 13:12:31 +0800

    updated .env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,9 @@ async function getCookieData() {
 }
 
 export default async function Home() {
-  const connection = await tRPC.connect.query().then((res) => res);
-  await getCookieData();
+  const cookieData = await getCookieData();
+  console.log(typeof cookieData);
 
+  const connection = await tRPC.connect.query().then((res) => res);
   return <div className="h-screen bg-indigo-300">{connection.status}</div>;
 }
