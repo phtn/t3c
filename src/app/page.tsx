602ad0968bf3a4e79f675435b4f63bf7bd911856
Jun Orville Lecena <jun.lecena@comptrolla.com>
2024-01-07 13:06:05 +0800

    added getCookieData

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import { tRPC } from "@@trpc/rsc";
+import { cookies } from "next/headers";
+
+async function getCookieData() {
+  const cookieData = cookies().getAll();
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(cookieData);
+    }, 1000),
+  );
+}
 
 export default async function Home() {
   const connection = await tRPC.connect.query().then((res) => res);
+  await getCookieData();
 
   return <div className="h-screen bg-indigo-300">{connection.status}</div>;
 }
